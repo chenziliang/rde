1079cec85a07e7cbc089337e349c3e4f8911e371
Ken Chen <zlchen.ken@gmail.com>
2021-08-23 21:24:33 -0700

    setup

--- setup.sh
+++ setup.sh
@@ -9,14 +9,20 @@ git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$H
 sed -i"" "/^  'completion'/a \  'git' \\\\\n  'syntax-highlighting' \\\\" ~/.zprezto/runcoms/zpreztorc
 sed -i"" "s/key-bindings 'emacs'/key-bindings 'vi'/g" ~/.zprezto/runcoms/zpreztorc
 
-# update ~/.zprezto/runcoms/zpreztorc
-# zstyle ':prezto:load' pmodule
-# ...
-#   'completion' \
-#   'git' \
-#   'syntax-highlighting' \
-#   'history-substring-search' \
-#   'prompt'
-
-# zstyle ':prezto:module:editor' key-bindings 'vi'
-# zstyle ':prezto:module:git:status:ignore' submodules 'all'
+rde()
+{
+    if [ $# -eq 1 ]; then
+        if [ -d "$1" ]; then
+            echo "Mapping $1 to /root"
+            docker run -v "$1":/root -w /root --cap-add=sys_nice --hostname λ -it rde;
+        fi
+    elif [ $# -eq 2 ]; then
+        if [ -d "$1" -a -d "$2" ]; then
+            base=`basename $2`
+            echo "Mapping $1 to /root and $2 to /$base"
+            docker run -v "$1":/root -w /root -v "$2":"/$base" --cap-add=sys_nice --hostname λ -it rde;
+        fi
+    else
+        docker run --cap-add=sys_nice --hostname λ -it rde;
+    fi
+}
